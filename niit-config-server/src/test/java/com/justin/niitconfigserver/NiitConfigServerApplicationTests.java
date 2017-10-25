0107ae6bd98227d040921fedb26a6738e255538a
Jackie2330489 <qd2017hst@163.com>
2017-10-25 09:29:30 +0800

    add /yml

--- niit-config-server/src/test/java/com/justin/niitconfigserver/NiitConfigServerApplicationTests.java
+++ niit-config-server/src/test/java/com/justin/niitconfigserver/NiitConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.justin.niitsecurity;
+package com.justin.niitconfigserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class NiitSecurityApplicationTests {
+public class NiitConfigServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
