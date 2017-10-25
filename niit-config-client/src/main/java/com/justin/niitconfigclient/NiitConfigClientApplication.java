98ba74484b2ec4dbce40fcd712d4d4cd832b945b
Jackie2330489 <qd2017hst@163.com>
2017-10-25 09:43:12 +0800

    add niit-config-client

---
+++ niit-config-client/src/main/java/com/justin/niitconfigclient/NiitConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.justin.niitconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiitConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiitConfigClientApplication.class, args);
+	}
+}
